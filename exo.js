4043b0246aa581efa000f2041cedcd970c0bec9b
youssefshadow <youssef20ben@gmail.com>
2023-01-16 15:54:48 +0100

    exo2

--- exo.js
+++ exo.js
@@ -1,2 +1,24 @@
 const collection = document.getElementsByTagName("p");
 console.log(collection[2]);
+collection[0].innerText='floflo';
+console.log(collection[0]);
+let title=document.getElementById("coolRaool");
+console.log(title);
+
+function ajouterTexte(pseudo, texte) {
+    let parag = document.createElement("p");
+    let strong = document.createElement("strong");
+    strong.textContent = pseudo;
+    parag.appendChild(strong);
+    let textNode = document.createTextNode(` ${texte}`);
+    parag.appendChild(textNode);
+    document.body.appendChild(parag);
+}
+  
+
+  ajouterTexte('florent','est de toute beauté');
+  ajouterTexte('JOsé','Gracia');
+  ajouterTexte('Grigny','La grande Borne');
+  ajouterTexte('Roi','Heenok');
+  ajouterTexte('Dongue','Rodrigue');
+ 
--- test2.js
+++ test2.js
@@ -10,6 +10,44 @@ newItem.appendChild(button);
 document.getElementById("myUL").appendChild(newItem);
 document.getElementById("myInput").value = "";
 
+function maFonction(pseudo, duTexte) {
+    // Créer un élément p
+    let parag = document.createElement("p");
+    
+    // Créer un élément strong pour afficher le pseudo en gras
+    let strong = document.createElement("strong");
+    strong.innerText = pseudo;
+    
+    // Ajouter le pseudo en gras dans le paragraphe
+    p.appendChild(strong);
+    
+    // Ajouter le texte donné dans le paragraphe
+    parag.innerHTML += " " + duTexte;
+    
+    // Ajouter le paragraphe dans la page
+    document.body.appendChild(parag);
+  }
+  
+
+  maFonction('florent','est de toute beauté');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
