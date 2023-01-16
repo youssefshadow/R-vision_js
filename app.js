30099bc856eb94be9cb16618a70ee003164063b6
youssefshadow <youssef20ben@gmail.com>
2023-01-16 12:01:14 +0100

    test_todolist

--- app.js
+++ app.js
@@ -1,4 +1,17 @@
-
+function newElement() {
+  let inputValue = document.getElementById("myInput").value;
+  if (inputValue === '') {
+      // si le champs est vide j'envoi une alerte
+      alert("Le champs est vide!");
+      return;
+  }
+  let newItem = "<li>" + inputValue + " <button onclick='removeElement(this)'>supprimer</button></li>";
+  document.getElementById("myUL").innerHTML += newItem;
+  document.getElementById("myInput").value = "";
+}
+function removeElement(element) {
+  element.parentNode.remove();
+}
 let list = document.querySelector('ul');
 list.addEventListener('click', toggleChecked, false);
 // foncion pour checker les elements
@@ -7,43 +20,3 @@ function toggleChecked(event) {
     event.target.classList.toggle('checked');
   }
 }
-// fonction ajout élément 
-function newElement() {
-    let inputValue = document.getElementById("myInput").value;
-    if (inputValue === '') {
-        // si le champs est vide j'envoi une alerte
-      alert("Le champs est vide!");
-      return;
-    }
-    
-    let li = createListItem(inputValue);
-    document.getElementById("myUL").appendChild(li);
-    document.getElementById("myInput").value = "";
-    for (i = 0; i < close.length; i++) {
-        close[i].onclick = function() {
-          let div = this.parentElement;
-          div.style.display = "none";
-        }
-      }
-  }
-
-  function createListItem(inputValue) {
-    let li = document.createElement("li");
-    let t = document.createTextNode(inputValue);
-    li.appendChild(t);
-    let span = document.createElement("SPAN");
-    let txt = document.createTextNode("supprimer");
-    span.className = "close";
-    span.appendChild(txt);
-    li.appendChild(span);
-    return li;
-  }
-  let close = document.getElementsByClassName("close");
-    let i;
-    for (i = 0; i < close.length; i++) {
-    close[i].onclick = function() {
-        let div = this.parentElement;
-        div.remove();
-    }
-}
-  
